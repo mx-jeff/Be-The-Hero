49a54df75073cd0c1531f504bfc9edbf062de5c4
Jeferson <mx,jeferson.12@gmail.com>
2020-03-25 12:32:57 -0300

    Backend: Done!

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -2,7 +2,22 @@ const connection = require('../database/connection')
 
 module.exports = {
     async index(request, response){
-        const incidents = await connection('incidents').select('*')
+        const { page = 1 } = request.query
+
+        const [count] = await connection('incidents').count()
+
+        response.header('X-total-Count', count['count(*)'])
+
+        const incidents = await connection('incidents')
+            .join('ongs','ong_id','=','incidents.ong_id')
+            .limit(5).offset((page -1) * 5)
+            .select([
+                'incidents.*',
+                'ongs.name',
+                'ongs.email',
+                'ongs.whatsapp',
+                'ongs.uf'
+            ])
 
         return response.json(incidents)
     },
@@ -24,12 +39,14 @@ module.exports = {
 
     async delete(request, response) {
         const { id } = request.params
-        const { ong_id } = request.headers.authorization
+        const ong_id = request.headers.authorization
 
         const incident = await connection('incidents')
         .where('id', id)
         .select('ong_id')
         .first()
+
+        console.log(incident.ong_id, ong_id)
 
         if (incident.ong_id != ong_id){
             return response.status(401).json({ error: "Operation not permitted"})
